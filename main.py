2c6d109515d0441cae87495d1131ffb16fe7fa46
Guanran928 <68757440+Guanran928@users.noreply.github.com>
2022-01-23 22:42:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from pynput.keyboard import Key, Controller
+import linecache  # read txt file
 import random  # for random line in txt file & random delay
 import time  # for delay
-import linecache  # read txt file
+
 import easygui  # mode selection
+from pynput.keyboard import Key, Controller
+
 # todo: replace easygui w/ tk & put goal and mode selection button in a single gui
 
 keyboard = Controller()
-pressGoal = easygui.enterbox('goal')
+pressGoal = easygui.enterbox("Repeat Time")
 
-if pressGoal is None:
-    quit()
-else:
-    pressGoal = int(pressGoal)  # 转换非'None'的值
-
+try:
+    if pressGoal is None:
+        quit()
+    else:
+        pressGoal = int(pressGoal)  # 转换非'None'的值
+except ValueError:
+    easygui.msgbox("Please fucking enter a number u idiot")  # :D yes swearing is good
 mode = easygui.buttonbox('AutoL v2',
-                         'AutoL v2 mode selection', ['L + text', 'L', 'text', 'in game L + text', 'in game text', 'exit'])
+                         'AutoL v2 mode selection',
+                         ['L + text', 'L', 'text', 'in game L + text', 'in game text', 'exit'])
 if mode == 'exit':
     quit()
 
@@ -34,25 +39,25 @@ if mode == 'L + text':
         keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800)))
         keyboard.press(Key.enter)
         time.sleep(delay)
-if mode == 'L':
+elif mode == 'L':
     for i in range(1, pressGoal):
         keyboard.type(prefix)
         keyboard.press(Key.enter)
         time.sleep(delay)
-if mode == 'text':
+elif mode == 'text':
     for i in range(1, pressGoal):
         keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800)))
         keyboard.press(Key.enter)
         time.sleep(delay)
-if mode == 'in game L + text':
+elif mode == 'in game L + text':
     for i in range(1, pressGoal):
         keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800)))
-        time.sleep(delay)  # 给minecraft弄的delay，不知道为什么，我在49，51行之间不加delay就不管用
+        time.sleep(delay)  # 给minecraft弄的delay，不知道为什么，我在52，54行之间不加delay就不管用
         keyboard.press(Key.enter)
         time.sleep(delay)
         keyboard.type(inGameChatButton)
         time.sleep(delay)
-if mode == 'in game text':
+elif mode == 'in game text':
     for i in range(1, pressGoal):
         keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800)))
         time.sleep(delay)
