43dfaf25311f9fd230066f8cf969931763b8b98d
Guanran928 <68757440+Guanran928@users.noreply.github.com>
2022-05-16 16:09:17 +0800

    Update lite_zh-cn_ver.py
    reformat

--- lite_zh-cn_ver.py
+++ lite_zh-cn_ver.py
@@ -1,81 +1,91 @@
+# coding=utf-8
 # Made By Guanran928 & nongbaobao(herry-gitee)
 # :D
-from timeit import repeat
-from pynput.keyboard import Key, Controller
 import linecache  # read txt file
 import random  # for random line in txt file & random delay
 import time  # for delay
 
+from pynput.keyboard import Key, Controller
+
+# init
 keyboard = Controller()
 initmode = ['前缀 + 句子', '句子', '游戏内前缀 + 句子 (自动按说话按钮)', '游戏内句子', '刷屏', '游戏内刷屏']
 
-def fuckUser(fvar, question, fuck1, fuck2):
+
+# settings
+def fuck_user(question, fuck1, fuck2, reform_str):
     while True:
-        fvar = None
-        fvar = input(question)
+        fuck_var = input(question)
         try:
-            if fvar == '':
-                exec(fuck1)
+            if fuck_var == '':
+                exec fuck1
             else:
+                exec reform_str  # transform failed (?)
                 break
         except ValueError:
-            exec(fuck2)  # :D yes swearing is good
-    return fvar
+            exec fuck2  # :D yes swearing is good
+    return fuck_var
+
 
 mode = input(initmode)
 if mode not in initmode:
-    print('nothing inputed, too lazy to make a loop to make u enter again')
+    print('nothing inputted, too lazy to make a loop to make u enter again')
     exit()
 
-if mode == initmode[0] or mode == initmode[2]: # does this work even
+if mode == initmode[0] or mode == initmode[2]:  # does this work even
     prefix = input('输入你想发送的前缀 默认为[FoodByte]: ')
     if prefix == '':
-            prefix = '[FoodByte]'
-            print (prefix)
+        prefix = '[FoodByte]'
+        print (prefix)
 
 if mode == initmode[4] or mode == initmode[5]:
-    spam = input('输入你想发送的句子: ')
+    while True:
+        spam = input('输入你想发送的句子: ')
+        if spam != '':
+            break
 
 inGameChatButton = 't'
 
-repeatTime = int(fuckUser('repeatTime', '输入你想发送的重复次数: ', 'print(\'大傻逼请宁输一个数\')', 'print(\'大傻逼请宁输一个整数\')'))
-delay = float(fuckUser('delay', '延迟(秒, Buggy): ', 'fvar = 0.5', 'print(\'大傻逼请宁输一个整数\')'))
+repeatTime = fuck_user('输入你想发送的重复次数: ', 'print(\'大傻逼请宁输一个数\')', 'print(\'大傻逼请宁输一个整数\')', 'int(fuck_var)')
+delay = fuck_user('延迟(秒, Buggy): ', 'fvar = 0.5', 'print(\'大傻逼请宁输一个整数\')', 'float(fuck_var)')
 # also you can use: delay = random.uniform(0.5, 1) to prevent BOT Detection
 
 # waiting for user to enter the fucking wechat
 time.sleep(2)
 
+
 # start spamming
-# SIMPLEFIED YES YES YES
-def repeatLoop(type, ingame):
-    if ingame == False:
+# SIMPLIFIED YES YES YES
+def spam_loop(spam_type, if_ingame):
+    if if_ingame is False:
         for i in range(repeatTime):
-            exec(type)
+            exec spam_type
             keyboard.press(Key.enter)
             time.sleep(delay)
         else:
             for i in range(repeatTime):
-                exec(type)
+                exec spam_type
                 time.sleep(delay)
                 keyboard.press(Key.enter)
                 time.sleep(delay)
                 keyboard.type(inGameChatButton)
                 time.sleep(delay)
 
+
 if mode == initmode[0]:
-    repeatLoop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), False)
+    spam_loop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), False)
 
 elif mode == initmode[1]:
-    repeatLoop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), False)
+    spam_loop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), False)
 
 elif mode == initmode[2]:
-    repeatLoop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), True)
+    spam_loop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), True)
 
 elif mode == initmode[3]:
-    repeatLoop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), True)
+    spam_loop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), True)
 
 elif mode == initmode[4]:
-    repeatLoop(keyboard.type(spam), False)
+    spam_loop(keyboard.type(spam), False)
 
 elif mode == initmode[5]:
-    repeatLoop(keyboard.type(spam), True)
+    spam_loop(keyboard.type(spam), True)
