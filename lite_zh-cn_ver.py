3fbd631ba8ff591cdd513b802b254c0d645b965d
Guanran928 <68757440+Guanran928@users.noreply.github.com>
2022-05-17 00:43:29 +0800

    Bug hotfix

--- lite_zh-cn_ver.py
+++ lite_zh-cn_ver.py
@@ -13,18 +13,20 @@ initmode = ['前缀 + 句子', '句子', '游戏内前缀 + 句子 (自动按说
 
 
 # settings
-def fuck_user(question, fuck1, fuck2, reform_str):
+def fuck_user(question, fuck1, fuck2, reform_str, ifInit = False, initValue = ''):
     while True:
         fuck_var = input(question)
         try:
             if fuck_var == '':
-                exec fuck1
+                exec(fuck1)
+                if ifInit == True:
+                    return initValue
             else:
-                exec reform_str  # transform failed (?)
+                exec(reform_str)
                 break
         except ValueError:
-            exec fuck2  # :D yes swearing is good
-    return fuck_var
+            exec(fuck2)  # :D yes shitcode
+    return eval(reform_str)
 
 
 mode = input(initmode)
@@ -41,13 +43,16 @@ if mode == initmode[0] or mode == initmode[2]:  # does this work even
 if mode == initmode[4] or mode == initmode[5]:
     while True:
         spam = input('输入你想发送的句子: ')
+        print(spam)
         if spam != '':
             break
+        else:
+            print('please enter smth')
 
 inGameChatButton = 't'
 
 repeatTime = fuck_user('输入你想发送的重复次数: ', 'print(\'大傻逼请宁输一个数\')', 'print(\'大傻逼请宁输一个整数\')', 'int(fuck_var)')
-delay = fuck_user('延迟(秒, Buggy): ', 'fvar = 0.5', 'print(\'大傻逼请宁输一个整数\')', 'float(fuck_var)')
+delay = fuck_user('延迟(秒, Buggy, 默认0.5s): ', 'print(\'0.5\')', 'print(\'大傻逼请宁输一个整数\')', 'float(fuck_var)', True, 0.5)
 # also you can use: delay = random.uniform(0.5, 1) to prevent BOT Detection
 
 # waiting for user to enter the fucking wechat
@@ -57,32 +62,34 @@ time.sleep(2)
 # start spamming
 # SIMPLIFIED YES YES YES
 def spam_loop(spam_type, if_ingame):
+    print('spamming started')
     if if_ingame is False:
         for i in range(repeatTime):
-            exec spam_type
+            eval(spam_type)
             keyboard.press(Key.enter)
             time.sleep(delay)
         else:
             for i in range(repeatTime):
-                exec spam_type
+                eval(spam_type)
                 time.sleep(delay)
                 keyboard.press(Key.enter)
                 time.sleep(delay)
                 keyboard.type(inGameChatButton)
                 time.sleep(delay)
+    print('spamming ended')
 
 
 if mode == initmode[0]:
-    spam_loop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), False)
+    spam_loop('keyboard.type(prefix + \' \' + linecache.getline(r\'800.txt\', random.randrange(1, 800)))', False)
 
 elif mode == initmode[1]:
-    spam_loop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), False)
+    spam_loop('keyboard.type(linecache.getline(r\'800.txt\', random.randrange(1, 800)))', False)
 
 elif mode == initmode[2]:
-    spam_loop(keyboard.type(prefix + ' ' + linecache.getline(r'800.txt', random.randrange(1, 800))), True)
+    spam_loop('keyboard.type(prefix + \' \' + linecache.getline(r\'800.txt\', random.randrange(1, 800)))', True)
 
 elif mode == initmode[3]:
-    spam_loop(keyboard.type(linecache.getline(r'800.txt', random.randrange(1, 800))), True)
+    spam_loop('keyboard.type(linecache.getline(r\'800.txt\', random.randrange(1, 800)))', True)
 
 elif mode == initmode[4]:
     spam_loop(keyboard.type(spam), False)
